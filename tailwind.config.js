a07b7a2cad98bfa47493fcaf581fa4189828c6a2
KacperFiga <kacper.figa.com@o2.pl>
2024-07-09 14:41:29 +0200

    style register and login page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      '.templates/user_manager/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
