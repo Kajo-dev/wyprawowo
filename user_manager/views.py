7ba2e047d7e7a097e658864f6e6a445ac5022449
Kajo <KajetanDyrcz@proton.me>
2024-07-20 12:46:38 +0200

    login this MF after success email conf

--- user_manager/views.py
+++ user_manager/views.py
@@ -28,6 +28,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
+        login(request, user)
         return redirect('login_page')
     else:
         error_list.append('Link jest niepoprawny')
