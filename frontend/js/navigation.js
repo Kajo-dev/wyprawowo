0314a20e32dfdf9393d1ce0e121cbc6caf8eaa0c
KacperFiga <kacper.figa.com@o2.pl>
2024-08-15 19:51:51 +0200

    add desktop menu

--- frontend/js/navigation.js
+++ frontend/js/navigation.js
@@ -12,4 +12,26 @@ if (window.innerWidth < 768) {
     closeButton.addEventListener('click', () => {
         navigation.classList.remove('active');
     })
+} else {
+    const profileAvatar = document.querySelector('.btn-avatar-option')
+    const closeButtonDesktop = document.querySelector('.btn-close');
+    const navigation = document.querySelector('.navbar-desktop-menu');
+    const activityPanel = document.querySelector('.activity-panel')
+
+    profileAvatar.addEventListener('click', () => {
+        navigation.classList.toggle('active');
+        activityPanel.addEventListener('click', closeMenu)
+    })
+
+    closeButtonDesktop.addEventListener('click', () => {
+        navigation.classList.remove('active');
+        activityPanel.addEventListener('click', closeMenu)
+    })
+
+    const closeMenu = () => {
+        navigation.classList.remove('active');
+        activityPanel.removeEventListener('click', () => {
+        })
+    }
 }
+
