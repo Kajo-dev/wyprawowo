87f44530aee6eeee6dffb85df96e7631e2621e49
Kajo <KajetanDyrcz@proton.me>
2024-09-24 19:37:05 +0200

    ja push

--- Wyprawowo/settings.py
+++ Wyprawowo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SITE_URL = 'https://journify-landing-zb56z4tuja-lm.a.run.app/'
-SITE_URL = 'http://34.118.108.240:8080/'
+SITE_URL = 'http://34.118.108.240:8000/'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
